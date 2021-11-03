128e94a6202f06c6e9ee3e2e5e3446e4739bbdba
Саша Прилуцкий <ififirfifir@gmail.com>
2021-11-03 23:05:11 +0300

    Добавьте файлы проекта.

---
+++ Yravnenie1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yravnenie1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double min = 0, max = 10;
+            const double toch = 0.0001;
+            double maxst = 1000;
+            double sr = 0;
+            int count = 0;
+            while (max - min > toch && count < maxst)
+            {
+                sr = (min + max) / 2;
+                double Fsr = F(sr);
+                double Fa = F(min);
+                double Fb = F(max);
+                if ((Fa * Fsr) <= 0)
+                {
+                    max = sr;
+                    Fb = Fsr;
+                }
+                else
+                {
+                    min = sr;
+                    Fa = Fsr;
+                }
+            }
+            Console.WriteLine(sr);
+            Console.ReadKey();
+        }
+        static double F(double x)
+        {
+            return x * Math.Sin(Math.Log(x));
+        }
+    }
+}
